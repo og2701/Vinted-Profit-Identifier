274c4376eebc6b33ad55e87edf5a25a39e5b226b
og2701 <67282414+og2701@users.noreply.github.com>
2025-08-05 17:22:58 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -166,7 +166,7 @@ def get_cex_buy_price(driver, query, vinted_item_details, log_messages):
 
         try:
             WebDriverWait(driver, 15).until(
-                EC.visibility_of_element_located((By.XPATH, "//h1"))
+                EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Trade-in value')]"))
             )
             time.sleep(0.5)
             page_html = driver.page_source
@@ -187,6 +187,9 @@ def get_cex_buy_price(driver, query, vinted_item_details, log_messages):
                 return {"price": cash_price, "link": driver.current_url}
             else:
                 log_messages.append("-> CeX: Could not find price in page HTML.")
+                log_messages.append("--- DEBUG: Page HTML at Price Failure ---")
+                log_messages.append(driver.page_source)
+                log_messages.append("--- END DEBUG ---")
                 return None
         except TimeoutException:
             log_messages.append("-> CeX: Timed out waiting for trade-in section.")
